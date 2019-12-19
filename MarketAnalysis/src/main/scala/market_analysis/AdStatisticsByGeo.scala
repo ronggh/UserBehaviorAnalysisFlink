c5d95b9abbb72b13d9501c6dff650b04ce66936e
ronggh@sina.cn <ronggh@sina.cn>
2019-12-19 11:14:42 +0800

    OrderPayDetect

--- MarketAnalysis/src/main/scala/market_analysis/AdStatisticsByGeo.scala
+++ MarketAnalysis/src/main/scala/market_analysis/AdStatisticsByGeo.scala
@@ -40,7 +40,7 @@ object AdStatisticsByGeo {
     // 自定义process function，过滤大量刷点击的行为
     val filterBlackListStream = adEventStream
       .keyBy( data => (data.userId, data.adId) )
-      .process( new FilterBlackListUser(100) )
+      .process( new FilterBlackListUser(100,blackListOutputTag) )
 
     // 根据省份做分组，开窗聚合
     val adCountStream = filterBlackListStream
@@ -51,51 +51,51 @@ object AdStatisticsByGeo {
     adCountStream.print("count")
     filterBlackListStream.getSideOutput(blackListOutputTag).print("blacklist")
 
+    //
     env.execute("ad statistics job")
   }
+}
 
-  // 内部类
-  class FilterBlackListUser(maxCount: Int) extends KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]{
-    // 定义状态，保存当前用户对当前广告的点击量
-    lazy val countState: ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]("count-state", classOf[Long]))
-    // 保存是否发送过黑名单的状态
-    lazy val isSentBlackList: ValueState[Boolean] = getRuntimeContext.getState( new ValueStateDescriptor[Boolean]("issent-state", classOf[Boolean]) )
-    // 保存定时器触发的时间戳
-    lazy val resetTimer: ValueState[Long] = getRuntimeContext.getState( new ValueStateDescriptor[Long]("resettime-state", classOf[Long]) )
+class FilterBlackListUser(maxCount: Int,blackListOutputTag:OutputTag[BlackListWarning]) extends KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]{
+  // 定义状态，保存当前用户对当前广告的点击量
+  lazy val countState: ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]("count-state", classOf[Long]))
+  // 保存是否发送过黑名单的状态
+  lazy val isSentBlackList: ValueState[Boolean] = getRuntimeContext.getState( new ValueStateDescriptor[Boolean]("issent-state", classOf[Boolean]) )
+  // 保存定时器触发的时间戳
+  lazy val resetTimer: ValueState[Long] = getRuntimeContext.getState( new ValueStateDescriptor[Long]("resettime-state", classOf[Long]) )
 
-    override def processElement(value: AdClickEvent, ctx: KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]#Context, out: Collector[AdClickEvent]): Unit = {
-      // 取出count状态
-      val curCount = countState.value()
+  override def processElement(value: AdClickEvent, ctx: KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]#Context, out: Collector[AdClickEvent]): Unit = {
+    // 取出count状态
+    val curCount = countState.value()
 
-      // 如果是第一次处理，注册定时器，每天00：00触发
-      if( curCount == 0 ){
-        val ts = ( ctx.timerService().currentProcessingTime()/(1000*60*60*24) + 1) * (1000*60*60*24)
-        resetTimer.update(ts)
-        ctx.timerService().registerProcessingTimeTimer(ts)
-      }
-
-      // 判断计数是否达到上限，如果到达则加入黑名单
-      if( curCount >= maxCount ){
-        // 判断是否发送过黑名单，只发送一次
-        if( !isSentBlackList.value() ){
-          isSentBlackList.update(true)
-          // 输出到侧输出流
-          ctx.output( blackListOutputTag, BlackListWarning(value.userId, value.adId, "Click over " + maxCount + " times today.") )
-        }
-        return
-      }
-      // 计数状态加1，输出数据到主流
-      countState.update( curCount + 1 )
-      out.collect( value )
+    // 如果是第一次处理，注册定时器，每天00：00触发
+    if( curCount == 0 ){
+      val ts = ( ctx.timerService().currentProcessingTime()/(1000*60*60*24) + 1) * (1000*60*60*24)
+      resetTimer.update(ts)
+      ctx.timerService().registerProcessingTimeTimer(ts)
     }
 
-    override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]#OnTimerContext, out: Collector[AdClickEvent]): Unit = {
-      // 定时器触发时，清空状态
-      if( timestamp == resetTimer.value() ){
-        isSentBlackList.clear()
-        countState.clear()
-        resetTimer.clear()
+    // 判断计数是否达到上限，如果到达则加入黑名单
+    if( curCount >= maxCount ){
+      // 判断是否发送过黑名单，只发送一次
+      if( !isSentBlackList.value() ){
+        isSentBlackList.update(true)
+        // 输出到侧输出流
+        ctx.output( blackListOutputTag, BlackListWarning(value.userId, value.adId, "Click over " + maxCount + " times today.") )
       }
+      return
+    }
+    // 计数状态加1，输出数据到主流
+    countState.update( curCount + 1 )
+    out.collect( value )
+  }
+
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]#OnTimerContext, out: Collector[AdClickEvent]): Unit = {
+    // 定时器触发时，清空状态
+    if( timestamp == resetTimer.value() ){
+      isSentBlackList.clear()
+      countState.clear()
+      resetTimer.clear()
     }
   }
 }
--- README.md
+++ README.md
@@ -9,3 +9,11 @@
 ### AppMarketingByChannel ,App推广渠道统计
 ### AppMarketing App推广统计，不分渠道
 ### AdStatisticsByGeo 页面广告分析
+## LoginFailDetect 恶意登陆监测
+### LoginFail 登录失败监测
+### LoginFailWithCep 登录失败监测CEP（复杂事件处理）实现
+## OrderPayDetect 订单支付实时监控
+### OrderTimeout 使用CEP处理订单超时检测
+### OrderTimeoutWithoutCep 不使用CEP处理订单超时检测
+### TxMatchDetect  实时对帐
+### TxMatchByJoin  实时对帐Join实现
